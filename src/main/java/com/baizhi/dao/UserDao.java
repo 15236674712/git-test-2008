110e46ba1f0c4a35230699fdf190619540f0675e
zhangcn <15236674712@163.com>
2021-01-27 18:05:24 +0800

    小黑：用户模块添加一个查所有方法

--- src/main/java/com/baizhi/dao/UserDao.java
+++ src/main/java/com/baizhi/dao/UserDao.java
@@ -2,8 +2,12 @@ package com.baizhi.dao;
 
 import com.baizhi.entity.User;
 
+import java.util.List;
+
 public interface UserDao {
 
     User queryById(String id);
+
+    List<User> queryAll();
 
 }
