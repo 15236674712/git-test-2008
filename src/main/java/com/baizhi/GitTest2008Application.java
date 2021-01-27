d565a6c437d369be43f3c265aff969ac47031c53
zhangcn <15236674712@163.com>
2021-01-27 17:02:02 +0800

    项目第一次提交

---
+++ src/main/java/com/baizhi/GitTest2008Application.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTest2008Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitTest2008Application.class, args);
+    }
+
+}
